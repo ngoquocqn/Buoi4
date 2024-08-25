4de52d81637a52affae64c3bf5613d1d5551e124
Ngô Tấn Quốc <Ngotanquocqn@gmail.com>
2024-08-25 12:26:03 +0700

    First commit

---
+++ src/baitapbuoi4/BaiTapBuoi4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baitapbuoi4;
+
+/**
+ *
+ * @author Acer
+ */
+public class BaiTapBuoi4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
